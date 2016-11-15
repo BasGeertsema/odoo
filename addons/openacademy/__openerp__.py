0fe676065b71ee6fb281be48b5508c1cafb4deb0
Bas Geertsema <mail@basgeertsema.net>
2016-11-15 19:15:54 +0100

    Finished tutorial

--- addons/openacademy/__openerp__.py
+++ addons/openacademy/__openerp__.py
@@ -19,14 +19,18 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        # 'views/views.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/session_workflow.xml',
+        'views/session_board.xml',
         'views/templates.xml',
         'views/openacademy.xml',
+        'views/partner.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
